561ef73dc19dad0d229e306c64d21df1c110fb94
Tom <tom>
2016-08-29 12:19:15 +1200

    portfolio update

--- includes/banner.php
+++
@@ -1,7 +0,0 @@
-
-<section class="banner text-center text-white">
-	<div class="container">
-		<h2>Send me a mesage</h2>
-		<a href="mailto:caigertom@gmail.com" class="btn text-white">caigertom@gmail.com</a>
-	</div>
-</section>
